f6cf6ca0ada7bd79383251fe6019360edf5eb11c
eryk <e.szczepaniec@lightpixel.io>
2026-01-09 10:57:15 +0100

    refactor: optimzie theme switcher class assignment

--- src/layouts/Navigation.tsx
+++ src/layouts/Navigation.tsx
@@ -3,12 +3,14 @@ import { ThemeProps } from '../types/types';
 import ThemeSwitcher from '../components/ThemeSwitcher';
 
 const Navigation: React.FC<ThemeProps> = ({ theme, setTheme }) => {
+  const lightThemeClasses = 'bg-[#fcfcfcff] text-[#202c37ff]';
+  const darkThemeClasses = 'bg-[#202c37ff] text-[#fcfcfcff]';
+
   return (
     <nav
-      className={`px-4 py-[2rem] shadow-md ${theme === 'dark' ? 'bg-[#202c37ff] text-[#fcfcfcff]' : 'bg-[#fcfcfcff] text-[#202c37ff]'}`}
+      className={`px-4 py-[2rem] shadow-md ${theme === 'dark' ? darkThemeClasses : lightThemeClasses}`}
     >
       <div className="max-w-[95%] mx-auto flex flex-wrap justify-between items-center">
-        {' '}
         <strong className="text-md lg:text-lg">Where in the world?</strong>
         <ThemeSwitcher theme={theme} setTheme={setTheme} />
       </div>
