ef435fa895ac7e4490fd474623e92c079444889b
eryk <e.szczepaniec@lightpixel.io>
2026-01-09 10:04:55 +0100

    chore: clear project . Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
 
 const App = () => {
   return (
@@ -7,7 +6,6 @@ const App = () => {
       <div className="bg-white rounded-lg shadow-sm p-4 mb-4">
         <h1 className="text-xl font-bold text-center">🚀 Rick and Morty - Fan Service</h1>
       </div>
-      <Outlet />
     </div>
   );
 };
