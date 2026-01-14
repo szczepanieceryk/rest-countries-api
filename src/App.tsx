1763de1493b945035e8c41bf6c41985f678013c7
eryk <e.szczepaniec@lightpixel.io>
2026-01-14 14:40:16 +0100

    refactor: move api t calls function asignments to hooks

--- src/App.tsx
+++ src/App.tsx
@@ -8,51 +8,22 @@ import { Country } from './types/types';
 const App = () => {
   const [theme, setTheme] = useLocalStorage('theme', 'light');
   const [countries, setCountries] = useState<Country[]>([]);
-  const { fetchCountries } = useCountriesData();
+  const { handleRegionSelect, initialAllCountriesFetch } = useCountriesData();
 
   useEffect(() => {
-    // let mounted = true;
-
-    const load = async () => {
-      // setLoading(true);
-      // setError(null);
-      try {
-        const allCountriesURL =
-          'https://restcountries.com/v3.1/all?fields=name,population,region,capital,flags';
-        const data = await fetchCountries(allCountriesURL);
-        setCountries(data);
-        console.log('Fetched countries:', data);
-        // if (mounted) setCountries(data);
-      } catch (err) {
-        // if (mounted) setError((err as Error).message);
-      } finally {
-        // if (mounted) setLoading(false);
-      }
-    };
-
-    load();
-
-    return () => {
-      // mounted = false;
-    };
+    initialAllCountriesFetch(setCountries);
   }, []); // call on initial page load
-
-  const handleRegionSelect = async (region: string) => {
-    try {
-      const url = `https://restcountries.com/v3.1/region/${encodeURIComponent(region)}`;
-      const data = await fetchCountries(url);
-      setCountries(data);
-    } catch (err) {
-      console.error('Error fetching countries by region:', err);
-    }
-  };
 
   return (
     <div
       className={`min-h-screen ${theme === 'dark' ? 'bg-gray-800 text-white' : 'bg-white text-gray-800'}`}
     >
       <Navigation theme={theme} setTheme={setTheme} />
-      <CountriesDisplay theme={theme} apiData={countries} onRegionSelect={handleRegionSelect} />
+      <CountriesDisplay
+        theme={theme}
+        apiData={countries}
+        onRegionSelect={(region) => handleRegionSelect(region, setCountries)}
+      />
     </div>
   );
 };
--- src/hooks/useCountriesData.ts
+++ src/hooks/useCountriesData.ts
@@ -9,7 +9,7 @@ const useCountriesData = () => {
         throw new Error('Failed to fetch countries');
       }
 
-      const data = await response.json();
+      const data: Country[] = await response.json();
 
       console.log('Fetched countries:', data);
       return data;
@@ -19,7 +19,34 @@ const useCountriesData = () => {
     }
   };
 
-  return { fetchCountries };
+  const handleRegionSelect = async (
+    region: string,
+    setCountries: React.Dispatch<React.SetStateAction<Country[]>>,
+  ) => {
+    try {
+      const url = `https://restcountries.com/v3.1/region/${encodeURIComponent(region)}`;
+      const data = await fetchCountries(url);
+      setCountries(data);
+    } catch (err) {
+      console.error('Error fetching countries by region:', err);
+    }
+  };
+
+  const initialAllCountriesFetch = async (
+    setCountries: React.Dispatch<React.SetStateAction<Country[]>>,
+  ) => {
+    try {
+      const data = await fetchCountries(
+        'https://restcountries.com/v3.1/all?fields=name,population,region,capital,flags',
+      );
+
+      setCountries(data);
+    } catch (err) {
+      console.error('Error fetching all countries:', err);
+    }
+  };
+
+  return { handleRegionSelect, initialAllCountriesFetch };
 };
 
 export default useCountriesData;
