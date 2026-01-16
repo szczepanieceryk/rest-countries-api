c5f95b1d48c8367c638a3d3b38d9600e2d655f14
eryk <e.szczepaniec@lightpixel.io>
2026-01-16 14:01:36 +0100

    feat: add rounting to display country details on new page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Navigation from './layouts/Navigation';
 import useLocalStorage from './hooks/useLocalStorage';
 import CountriesDisplay from './layouts/CountriesDisplay';
 import useCountriesData from './hooks/useCountriesData';
 import { Country } from './types/types';
+import CountryDetails from './components/CountryDetails';
 
 const App = () => {
   const [theme, setTheme] = useLocalStorage('theme', 'light');
@@ -15,16 +18,26 @@ const App = () => {
   }, []); // call on initial page load
 
   return (
-    <div
-      className={`min-h-screen ${theme === 'dark' ? 'bg-gray-800 text-white' : 'bg-white text-gray-800'}`}
-    >
-      <Navigation theme={theme} setTheme={setTheme} />
-      <CountriesDisplay
-        theme={theme}
-        apiData={countries}
-        onRegionSelect={(region) => handleRegionSelect(region, setCountries)}
-      />
-    </div>
+    <BrowserRouter>
+      <div
+        className={`min-h-screen ${theme === 'dark' ? 'bg-gray-800 text-white' : 'bg-white text-gray-800'}`}
+      >
+        <Navigation theme={theme} setTheme={setTheme} />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <CountriesDisplay
+                theme={theme}
+                apiData={countries}
+                onRegionSelect={(region) => handleRegionSelect(region, setCountries)}
+              />
+            }
+          />
+          <Route path="/country/:name" element={<CountryDetails theme={theme} />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 };
 
