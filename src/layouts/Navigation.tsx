3aeee6796d65e47445bd0e9f1b32a83278b592c3
eryk <e.szczepaniec@lightpixel.io>
2026-01-20 13:48:27 +0100

    chore: go to main page on click in navigation "logo" placeholder

--- src/layouts/Navigation.tsx
+++ src/layouts/Navigation.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import { ThemeProps } from '../types/types';
 import ThemeSwitcher from '../components/ThemeSwitcher';
+import { useNavigate } from 'react-router-dom';
 
 const Navigation: React.FC<ThemeProps> = ({ theme, setTheme }) => {
   const lightThemeClasses = 'bg-[#fcfcfcff] text-[#202c37ff]';
   const darkThemeClasses = 'bg-[#2b3945ff] text-[#fcfcfcff]';
-
+  const navigate = useNavigate();
   return (
     <nav
       className={`px-4 py-[2rem] shadow-md ${theme === 'dark' ? darkThemeClasses : lightThemeClasses}`}
     >
       <div className="max-w-[95%] mx-auto flex flex-wrap justify-between items-center">
-        <strong className="text-md lg:text-lg">Where in the world?</strong>
+        <strong onClick={() => navigate('/')} className="text-md lg:text-lg cursor-pointer">
+          Where in the world?
+        </strong>
         <ThemeSwitcher theme={theme} setTheme={setTheme} />
       </div>
     </nav>
